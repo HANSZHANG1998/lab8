e240f8aaa7ca072c160fe16d54eef684a636d4bb
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-19 22:56:16 -0500

    Update initMode.sh

--- initMode.sh
+++ initMode.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-#COUNTER=0
-
-#while [ $COUNTER -lt 4 ] # while the counter is less than 4
-
-pins='0 1 2 3' #for loop takes a list of items
-for pins in $pins #for each of the items in the list do the following
+pins='0 1 2 3' 
+for pins in $pins 
 do
-		#echo $COUNTER # do this
-		gpio mode $pins out; #here  we set it to out
-		gpio write $pins 0; #here  0 is the value we set it to
 		
-		
-		#((COUNTER++)) # increment counter
-done
+		gpio mode $pins out;
+		gpio write $pins 0;
 
-#echo "All done"
-#echo "pins set to output and initalized to value 0 (off)"
+done
